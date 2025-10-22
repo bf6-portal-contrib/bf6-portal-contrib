7bab34a3c70ae9a8695e49cdb766e355be9f6f76
Manuel Pacheco <contact@manuelpacheco.dev>
2025-10-22 18:40:33 -0300

    remove ui-debug

--- packages/arms-race/src/index.ts
+++ packages/arms-race/src/index.ts
@@ -1,7 +1,5 @@
 export * from '@bf6-portal-contrib/gate/hooks';
 
 import { ArmsRace } from './arms-race.ts';
-import { UIDebug } from './ui-debug.ts';
 
 new ArmsRace().subscribe();
-new UIDebug().subscribe();
