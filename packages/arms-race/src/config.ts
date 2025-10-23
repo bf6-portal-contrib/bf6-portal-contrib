18d9e3ff09d979c0dbc4f1ca406f0bceb9e0a59e
Manuel Pacheco <contact@manuelpacheco.dev>
2025-10-23 02:30:18 -0300

    improve weapon list to 16 weapons, 2 per type

--- packages/arms-race/src/config.ts
+++ packages/arms-race/src/config.ts
@@ -1,24 +1,47 @@
+const weaponToInventory = (
+  weapon: mod.Weapons
+): Map<mod.InventorySlots, mod.Weapons> =>
+  new Map([[mod.InventorySlots.PrimaryWeapon, weapon]]);
+
 export const Config = {
   killsPerLevel: 2,
 
   levelInventories: [
-    // new Map([
-    //   [mod.InventorySlots.PrimaryWeapon, mod.Weapons.Sniper_M2010_ESR]
-    // ]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.DMR_SVDM]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.DMR_M39_EMR]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.DMR_M39_EMR]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.LMG_M_60]]),
-    new Map([
-      [mod.InventorySlots.PrimaryWeapon, mod.Weapons.AssaultRifle_M433],
-    ]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.Carbine_M4A1]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.SMG_SGX]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.SMG_PW5A3]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.Shotgun_M87A1]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.Shotgun_M1014]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.Sidearm_P18]]),
-    new Map([[mod.InventorySlots.PrimaryWeapon, mod.Weapons.Sidearm_M45A1]]),
+    ...[
+      // Snipers
+      mod.Weapons.Sniper_PSR,
+      mod.Weapons.Sniper_M2010_ESR,
+
+      // DMRs
+      mod.Weapons.DMR_SVK_86,
+      mod.Weapons.DMR_M39_EMR,
+
+      // LMGs
+      mod.Weapons.LMG_M240L,
+      mod.Weapons.LMG_M250,
+
+      // Assault Rifles
+      mod.Weapons.AssaultRifle_M433,
+      mod.Weapons.AssaultRifle_NVO_228E,
+
+      // Carbines
+      mod.Weapons.Carbine_M4A1,
+      mod.Weapons.Carbine_SG_553R,
+
+      // SMGs
+      mod.Weapons.SMG_SGX,
+      mod.Weapons.SMG_KV9,
+
+      // Shotguns
+      mod.Weapons.Shotgun_M87A1,
+      mod.Weapons.Shotgun_M1014,
+
+      // Sidearms
+      mod.Weapons.Sidearm_P18,
+      mod.Weapons.Sidearm_M45A1,
+    ].map(weaponToInventory),
+
+    // Knife
     new Map(),
   ] as Map<mod.InventorySlots, mod.Weapons | mod.Gadgets>[],
 
